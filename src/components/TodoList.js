400737325fd17a0f26f1ed7365fe333c5fd2ef08
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-11 16:45:32 +0430

    todo list component create

---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <div className='todoList mt-4'>
+            <div className='container'>
+                <div className='d-flex flex-column align-items-center'>
+                <nav className='col-6 mb-3'>
+                        <div className='nav nav-tabs' id='tab-nav' role="tablist">
+                            <a className="nav-item nav-link font-weight-bold" id="nav-home-tab">undone<span className='badge bg-warning rounded mx-1'>0</span ></a>
+                            <a className="nav-item nav-link font-weight-bold" id="nav-profile-tab">done<span className='badge bg-success rounded mx-1'>0</span ></a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TodoList;
