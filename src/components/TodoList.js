3c24e7f6aaf8d3ca94d637e66671abfc5262985a
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-11 19:36:51 +0430

    create a function for counting undone and done todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,14 +14,19 @@ const TodoList = () => {
 
     const filteredTodos = todos.filter(item => item.isDone === status);
 
+    const listLength = boolean => {
+        const length = todos.filter(item => item.isDone === boolean).length;
+        return length;
+    }
+
     return (
         <div className='todoList mt-4'>
             <div className='container'>
                 <div className='d-flex flex-column align-items-center'>
                     <nav className='col-6 mb-3'>
                         <div className='nav nav-tabs' id='tab-nav' role="tablist">
-                            <a className="nav-item nav-link font-weight-bold" id="nav-home-tab" onClick={() => setStatus(false)}>undone<span className='badge bg-warning rounded mx-1'>0</span ></a>
-                            <a className="nav-item nav-link font-weight-bold" id="nav-profile-tab" onClick={() => setStatus(true)}>done<span className='badge bg-success rounded mx-1'>0</span ></a>
+                            <a className="nav-item nav-link font-weight-bold" id="nav-home-tab" onClick={() => setStatus(false)}>undone<span className='badge bg-warning rounded mx-1'>{listLength(false)}</span ></a>
+                            <a className="nav-item nav-link font-weight-bold" id="nav-profile-tab" onClick={() => setStatus(true)}>done<span className='badge bg-success rounded mx-1'>{listLength(true)}</span ></a>
                         </div>
                     </nav>
                     {
