25cf49b3f7aabedbb66ca5e12c38ae9ebe25a0e4
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-11 16:54:04 +0430

    todo component is created

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Todo = () => {
+    return (
+        <div className='col-6 mb-2'>
+            <div className='d-flex justify-content-between align-items-center border border-light border-rounded p-3'>
+                <div>title</div>
+                <div>
+                    <button type="button" class="btn btn-sm btn-success">done</button>
+                    <button type="button" class="btn btn-sm btn-info mx-1 text-white">edit</button>
+                    <button type="button" class="btn btn-sm btn-danger">delete</button>
+                </div>
+        </div>
+        </div>
+    );
+};
+
+export default Todo;
