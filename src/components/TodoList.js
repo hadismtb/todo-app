f7675beb62b7bb9974870441fc4f5d51132a1e47
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-11 18:40:20 +0430

    make ready todo component for rendering and get the props

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,7 +14,7 @@ const TodoList = () => {
         <div className='todoList mt-4'>
             <div className='container'>
                 <div className='d-flex flex-column align-items-center'>
-                <nav className='col-6 mb-3'>
+                    <nav className='col-6 mb-3'>
                         <div className='nav nav-tabs' id='tab-nav' role="tablist">
                             <a className="nav-item nav-link font-weight-bold" id="nav-home-tab">undone<span className='badge bg-warning rounded mx-1'>0</span ></a>
                             <a className="nav-item nav-link font-weight-bold" id="nav-profile-tab">done<span className='badge bg-success rounded mx-1'>0</span ></a>
